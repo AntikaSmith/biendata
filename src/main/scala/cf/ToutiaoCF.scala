2dd3f639e9a9c0f3567ba5d3e342bac5e6ed4d9d
zhanghan <zhanghan@qiaobutang.com>
2016-09-14 10:58:38 +0800

    add the basic code

--- src/main/scala/cf/ToutiaoCF.scala
+++ src/main/scala/cf/ToutiaoCF.scala
@@ -6,7 +6,46 @@ package cf
 
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.recommendation.ALS
+import org.apache.spark.sql.SparkSession
 
 object ToutiaoCF {
 
+
+  case class Rating(questionId: String, userId: String, answer_? : Float)
+
+  def pasreRating(str: String): Rating = {
+    val fields = str.split("\t")
+    assert(fields.size == 3)
+    Rating(fields(0), fields(1), fields(2).toFloat)
+  }
+
+  def main(args: Array[String]) = {
+    val spark = SparkSession.builder().appName("toutiaocf").getOrCreate()
+
+    import spark.implicits._
+
+    val ratings = spark.read.textFile("invited_info_train.txt").map(pasreRating(_)).toDF()
+
+    val Array(training, test) = ratings.randomSplit(Array(0.8, 0.2))
+
+    val als = new ALS()
+      .setMaxIter(5)
+      .setRegParam(0.01)
+      .setUserCol("userId")
+      .setItemCol("questionId")
+      .setRatingCol("answer_?")
+
+    val model = als.fit(training)
+
+    val predictions = model.transform(test)
+    val evaluator = new RegressionEvaluator()
+      .setMetricName("rmse")
+      .setLabelCol("rating")
+      .setPredictionCol("prediction")
+    val rmse = evaluator.evaluate(predictions)
+    println(s"Root-mean-square error = $rmse")
+    // $example off$
+
+    spark.stop()
+  }
 }
