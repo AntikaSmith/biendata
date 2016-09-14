9541a6a87b4adde085e5cc435ad47e18423015a9
zhanghan <zhanghan@qiaobutang.com>
2016-09-14 09:52:18 +0800

    add the basic data

--- src/main/scala/cf/ToutiaoCF.scala
+++ src/main/scala/cf/ToutiaoCF.scala
@@ -3,6 +3,10 @@ package cf
 /**
   * Created by ZhangHan on 2016/9/14.
   */
+
+import org.apache.spark.ml.evaluation.RegressionEvaluator
+import org.apache.spark.ml.recommendation.ALS
+
 object ToutiaoCF {
 
 }
