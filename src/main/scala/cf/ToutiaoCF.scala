c7efdf6efe58a9a6ac4141319e509ee302fa82a7
zhanghan <zhanghan@qiaobutang.com>
2016-09-23 19:07:38 +0800

    神经网络预测结果太差，整合反而降低协同过滤性能，抛弃之

--- src/main/scala/cf/ToutiaoCF.scala
+++ src/main/scala/cf/ToutiaoCF.scala
@@ -33,7 +33,7 @@ object ToutiaoCF {
     val prediction = x(3).asInstanceOf[Float]
     if (prediction.isNaN){
       //println(s"unkown num:$x")
-      Prediction(x(0).asInstanceOf[Int], x(1).asInstanceOf[Int], x(2).asInstanceOf[Float], 0.01f)
+      Prediction(x(0).asInstanceOf[Int], x(1).asInstanceOf[Int], x(2).asInstanceOf[Float], 0.0f)
     }
     else
       Prediction(x(0).asInstanceOf[Int], x(1).asInstanceOf[Int], x(2).asInstanceOf[Float], math.min(x(3).asInstanceOf[Float], 1))
@@ -47,6 +47,8 @@ object ToutiaoCF {
     def constructData(fileName: String) = spark.read.textFile(fileName).map(pasreRating(_)).toDF()
 
     val training = constructData("src/main/resources/invited_info_train.txt")
+//    val ratings = spark.read.textFile("src/main/resources/invited_info_train.txt").map(pasreRating(_)).toDF()
+//    val Array(training, test) = ratings.randomSplit(Array(0.9, 0.1))
 
     val validating = constructData("src/main/resources/validate_nolabel.txt")
 
@@ -54,40 +56,41 @@ object ToutiaoCF {
       .setMaxIter(5)
       .setRegParam(0.01)
       .setRank(200)
-      .setUserCol("userId")
-      .setItemCol("questionId")
+      .setUserCol("questionId")
+      .setItemCol("userId")
       .setRatingCol("answer")
       .setNonnegative(true)
 
     val model = als.fit(training)
 
-    val predictions = model.transform(validating).map(convertProb).map(prediction =>
+    val predictions = model.transform(validating).map(convertProb)
+    val result = predictions.map(prediction =>
       Result(ParseUtil.numberId2qidMap(prediction.questionId), ParseUtil.numberId2uidMap(prediction.userId), prediction.prediction)
     )
-    predictions
+    result
       // place all data in a single partition
       .coalesce(1)
-      .write.format("com.databricks.spark.csv").mode(SaveMode.Overwrite)
+      .write.format("com.databricks.spark.csv").mode(SaveMode.Append)
       .option("header", "true")
       .save("data")
-    val nn_output = scala.io.Source.fromFile("nn_output.txt").mkString.split("\n").map(_.toFloat)
-    val cf_output = scala.io.Source.fromFile("data/final.csv").mkString.split("\n").map(_.split(",")).zip(nn_output).map{
-      case (arr, nn) if arr(0) != "qid"=>
-        Array(arr(0), arr(1), (arr(2).toFloat * 0.8 + nn * 0.2).toFloat).mkString(",")
-      case x =>
-        x._1.mkString(",")
-    }.mkString("", "\n", "\n")
-
-    val finalFile = new File("final.csv")
-    val writer = new PrintWriter(finalFile)
-    writer.write(cf_output)
-    writer.close()
-//    val evaluator = new RegressionEvaluator()
-//      .setMetricName("rmse")
-//      .setLabelCol("answer")
-//      .setPredictionCol("prediction")
-//    val rmse = evaluator.evaluate(predictions)
-//    println(s"Root-mean-square error = $rmse")
+//    val nn_output = scala.io.Source.fromFile("nn_output.txt").mkString.split("\n").map(_.toFloat)
+//    val cf_output = scala.io.Source.fromFile("data/final.csv").mkString.split("\n").map(_.split(",")).zip(nn_output).map{
+//      case (arr, nn) if arr(0) != "qid"=>
+//        Array(arr(0), arr(1), (arr(2).toFloat * 0.8 + nn * 0.2).toFloat).mkString(",")
+//      case x =>
+//        x._1.mkString(",")
+//    }.mkString("", "\n", "\n")
+//
+//    val finalFile = new File("final.csv")
+//    val writer = new PrintWriter(finalFile)
+//    writer.write(cf_output)
+//    writer.close()
+    val evaluator = new RegressionEvaluator()
+      .setMetricName("rmse")
+      .setLabelCol("answer")
+      .setPredictionCol("prediction")
+    val rmse = evaluator.evaluate(predictions)
+    println(s"Root-mean-square error = $rmse")
     // $example off$
 
     spark.stop()
