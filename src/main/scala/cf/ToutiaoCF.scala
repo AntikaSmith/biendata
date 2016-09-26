bb70faad6f4f0d2416787f1a2acfacd51eb57389
zhanghan <zhanghan@qiaobutang.com>
2016-09-26 13:33:15 +0800

    分别依据用户和问题来做协同过滤，然后集成起来

--- src/main/scala/cf/ToutiaoCF.scala
+++ src/main/scala/cf/ToutiaoCF.scala
@@ -8,8 +8,8 @@ import java.io.{BufferedReader, File, InputStreamReader, PrintWriter}
 
 import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.ml.recommendation.ALS
-import org.apache.spark.sql.{Dataset, Row, SaveMode, SparkSession}
-import util.ParseUtil
+import org.apache.spark.sql._
+import _root_.util.ParseUtil
 
 object ToutiaoCF {
 
@@ -18,7 +18,8 @@ object ToutiaoCF {
   case class Prediction(questionId: Int, userId: Int, answer : Float, prediction: Float)
   case class Result(qid: String, uid: String, label: Float)
 
-
+  val defualtValueLossMap1 = scala.collection.mutable.Map[Float, Double](0.01f -> 1)
+  val defualtValueLossMap2 = scala.collection.mutable.Map[Float, Double](0.01f -> 1)
 
   def pasreRating(str: String): Rating = {
     import ParseUtil.{qid2numberIdMap, uid2numberIdMap}
@@ -29,44 +30,80 @@ object ToutiaoCF {
     Rating(qid2numberIdMap(fields(0)), uid2numberIdMap(fields(1)), fields(2).toFloat)
   }
 
-  def convertProb(x: Row) = {
+  def convertProb(x: Row)(defaultValue: Float) = {
     val prediction = x(3).asInstanceOf[Float]
     if (prediction.isNaN){
       //println(s"unkown num:$x")
-      Prediction(x(0).asInstanceOf[Int], x(1).asInstanceOf[Int], x(2).asInstanceOf[Float], 0.0f)
+      Prediction(x(0).asInstanceOf[Int], x(1).asInstanceOf[Int], x(2).asInstanceOf[Float], defaultValue)
     }
     else
       Prediction(x(0).asInstanceOf[Int], x(1).asInstanceOf[Int], x(2).asInstanceOf[Float], math.min(x(3).asInstanceOf[Float], 1))
   }
 
-  def main(args: Array[String]) = {
-    val spark = ParseUtil.spark
-
-    import spark.implicits._
-
-    def constructData(fileName: String) = spark.read.textFile(fileName).map(pasreRating(_)).toDF()
-
-    val training = constructData("src/main/resources/invited_info_train.txt")
-//    val ratings = spark.read.textFile("src/main/resources/invited_info_train.txt").map(pasreRating(_)).toDF()
-//    val Array(training, test) = ratings.randomSplit(Array(0.9, 0.1))
-
-    val validating = constructData("src/main/resources/validate_nolabel.txt")
+  def modelAndTest(training: DataFrame, test: DataFrame, sparkSession: SparkSession, userStr: String, questionStr: String, map: scala.collection.mutable.Map[Float, Double]) = {
+    import sparkSession.implicits._
 
     val als = new ALS()
       .setMaxIter(5)
       .setRegParam(0.01)
       .setRank(200)
-      .setUserCol("questionId")
-      .setItemCol("userId")
+      .setUserCol(userStr)
+      .setItemCol(questionStr)
       .setRatingCol("answer")
       .setNonnegative(true)
 
     val model = als.fit(training)
+    var loss = 1.0
+    var tmp:Dataset[Prediction] = null
+    for (defaultValue <- map.keys){
+      val predictions: Dataset[Prediction] = model.transform(test).map(convertProb(_)(defaultValue))
+      val evaluator = new RegressionEvaluator()
+        .setMetricName("rmse")
+        .setLabelCol("answer")
+        .setPredictionCol("prediction")
+      val rmse = evaluator.evaluate(predictions)
+      map(defaultValue) = rmse
+      if (rmse < loss) {
+        tmp = predictions
+        loss = rmse
+      }
+    }
 
-    val predictions = model.transform(validating).map(convertProb)
+    tmp
+  }
+
+  def constructData(fileName: String, spark: SparkSession) = {
+    import spark.implicits._
+    spark.read.textFile(fileName).map(pasreRating(_)).toDF()
+  }
+
+  def main(args: Array[String]) = {
+    val spark: SparkSession = ParseUtil.spark
+
+    import spark.implicits._
+
+    val training: DataFrame = constructData("src/main/resources/invited_info_train.txt", spark)
+    val validating = constructData("src/main/resources/validate_nolabel.txt", spark)
+
+//    val ratings = spark.read.textFile("src/main/resources/invited_info_train.txt").map(pasreRating(_)).toDF()
+//    val Array(training, validating) = ratings.randomSplit(Array(0.9, 0.1))
+
+
+
+    val predictionBaseUser = modelAndTest(training, validating, spark, "userId", "questionId", defualtValueLossMap1)
+    val predictionBaseQuestion = modelAndTest(training, validating, spark, "questionId", "userId", defualtValueLossMap2)
+
+    predictionBaseQuestion.createOrReplaceTempView("Question")
+    predictionBaseUser.createOrReplaceTempView("User")
+    val predictions = spark.sql("select Question.questionId, Question.userId, Question.prediction+User.prediction " +
+      "from Question join User where Question.questionId=User.QuestionId and Question.userId=User.UserId").map(row =>
+      Prediction(row.getInt(0), row.getInt(1), 0.0f, row.getFloat(2))
+    )
+
     val result = predictions.map(prediction =>
       Result(ParseUtil.numberId2qidMap(prediction.questionId), ParseUtil.numberId2uidMap(prediction.userId), prediction.prediction)
     )
+
     result
       // place all data in a single partition
       .coalesce(1)
@@ -85,13 +122,8 @@ object ToutiaoCF {
 //    val writer = new PrintWriter(finalFile)
 //    writer.write(cf_output)
 //    writer.close()
-    val evaluator = new RegressionEvaluator()
-      .setMetricName("rmse")
-      .setLabelCol("answer")
-      .setPredictionCol("prediction")
-    val rmse = evaluator.evaluate(predictions)
-    println(s"Root-mean-square error = $rmse")
-    // $example off$
+
+
 
     spark.stop()
   }
